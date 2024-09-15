de392818ac0dccc9134618445ec1e19b0e9f40a2
Andrew <clovval@gmail.com>
2024-09-15 12:53:58 -0600

    revisited code and created entry level UI

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 var computerScore = 0;
 var playerScore = 0;
+const btn = document.querySelectorAll("button");
+const humScore = document.querySelector("#humanScore");
+const botScore = document.querySelector("#botScore");
+
 
 function getComputerChoice() {
     return Math.floor((Math.random() * 3) + 1);
@@ -17,59 +21,89 @@ const compChoiceHelper = () => {
     }
     return choiceString;
 };
-// Breakpoint for helper function 
-// console.log(compChoiceHelper(getComputerChoice()));
-
-const humanInput = () => {
-    let input = prompt("Enter your choice: (rock, paper, or scissors)");
-    let lowerCase = input.toLowerCase();
-    return lowerCase;
-};
-
-// breakpoint for humanInput
-//console.log(humanInput());
-
 
 // break point 
-//console.log(playRound(humanInput(), compChoiceHelper()));
+
 const playRound = (humanInput, compChoice) => {
-    if(compChoice == "rock" && humanInput == "rock" || compChoice == "paper" && humanInput == "paper" || compChoice == "scissors" && humanInput == "scissors")
+    console.log(humanInput, compChoice);
+
+    if(playerScore < 5 && computerScore < 5)
     {
-        console.log("Its a tie, try again. Computer chose: " + compChoice + " You chose: " + humanInput);
-        computerScore += 0;
-        playerScore += 0;
-    } else if(compChoice == "rock" && humanInput == "scissors" || compChoice == "scissors" && humanInput == "paper" || compChoice == "paper" && humanInput == "rock") {
-        console.log("Computer wins. You lose..." + compChoice + " You chose: " + humanInput);
-        computerScore++;
+        if(compChoice == "rock" && humanInput == "rock" || compChoice == "paper" && humanInput == "paper" || compChoice == "scissors" && humanInput == "scissors")
+        {
+            console.log("Its a tie, try again. Computer chose: " + compChoice + " You chose: " + humanInput);
+            computerScore += 0;
+            playerScore += 0;
+        } else if(compChoice == "rock" && humanInput == "scissors" || 
+                  compChoice == "scissors" && humanInput == "paper" || 
+                  compChoice == "paper" && humanInput == "rock") {
+                    
+            console.log("Computer wins. You lose..." + compChoice + " You chose: " + humanInput);
+            computerScore++;
+        }
+        else if(compChoice == "scissors" && humanInput == "rock" || 
+                compChoice == "paper" && humanInput == "scissors" || 
+                compChoice == "rock" && humanInput == "paper"){
+    
+            console.log("You win. Computer loses. Computer chose: " + compChoice + " you chose: " + humanInput);
+            playerScore++;
+        }
+        humScore.textContent = playerScore;
+        botScore.textContent = computerScore;
     }
-    else if(compChoice == "scissors" && humanInput == "rock" || compChoice == "paper" && humanInput == "scissors" || compChoice == "rock" && humanInput == "paper"){
-        console.log("You win. Computer loses. Computer chose: " + compChoice + " you chose: " + humanInput);
-        playerScore++;
+
+    if(playerScore === 5)
+    {
+        console.log("win");
+        endGame();
+            
     }
+    else if (computerScore === 5) {
+        console.log("lose");
+        endGame();
+    }
+
 };
 
-const playGame = () => {
-    var roundCounter = 0;
-    while(roundCounter != 5)
-    {
-        playRound(humanInput(), compChoiceHelper());
-        // breakpoint 
-        // console.log(roundCounter);
-        // console.log("Computer score: " + computerScore + " Your score: " + playerScore);
-        ++roundCounter;
-    }
-    if(playerScore > computerScore)
-    {
-        console.log("You won the game!");
-        console.log("Score: \nComputer: " + computerScore + " Player: " + playerScore)
-    } else if(computerScore > playerScore) {
-        console.log("Computer won the game!");
-        console.log("Score: \nComputer: " + computerScore + " Player: " + playerScore)
-    }
-    else {
-        console.log("Its a tie game!");
-        console.log("Score: \nComputer: " + computerScore + " Player: " + playerScore)
-    }
+const endGame = () => {
+    btn.forEach(btn => btn.style.display = "none");
+    document.getElementById("restart").style.display = "block";
 };
 
-playGame();
+const resetGame = () => {
+    playerScore = 0;
+    computerScore = 0;
+    humScore.textContent = playerScore;
+    botScore.textContent = computerScore;
+
+    btn.forEach(btn => btn.style.display = "");
+    document.getElementById("restart").style.display = "none";
+    document.getElementById("start").style.display = "none";
+};
+
+
+
+
+const play = () => {
+    // logic to disable headers and buttons before start game pressed
+    btn.forEach(btn => btn.style.display = "none");
+    document.getElementById("head").style.display = "none";
+    document.getElementById("start").style.display = "flex";
+
+    // Once startgame clicked disable button and unhide other buttons for game to start
+    document.getElementById("start").addEventListener("click", () => {
+        btn.forEach(btn => btn.style.display = "");
+        document.getElementById("start").style.display = "none";
+        document.getElementById("head").style.display = "";
+        document.getElementById("restart").style.display = "none";
+    })
+
+    btn.forEach(btn => {
+        btn.addEventListener("click", () => { // wait for user to press selection to call round function
+            playRound(btn.id, compChoiceHelper()); // use btn.id to grab id of buttons to compare to answers
+        })
+    });
+};
+
+document.getElementById("restart").addEventListener("click", resetGame);
+play();
